0811201c7fc3b7c2a721b72c78ca5b95155f729b
SabaAnwar <saba.anwar@xorbix.com>
2025-07-15 17:45:19 +0500

    Initial Commit

---
+++ FlowerFresh/FlowerFreshApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlowerFreshApp.swift
+//  FlowerFresh
+//
+//  Created by Saba Anwar on 15/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct FlowerFreshApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FlowerFreshUITests/FlowerFreshUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  FlowerFreshUITestsLaunchTests.swift
+//  FlowerFreshUITests
+//
+//  Created by Saba Anwar on 15/07/2025.
+//
+
+import XCTest
+
+final class FlowerFreshUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
